67d690f19e865406453772ea86a29fc3fc61650d
Julian Salomon <jesalomont@unal.edu.co>
2019-10-10 02:01:42 -0500

    + Added Animal API

---
+++ src/animal/typeDefs.js
@@ -0,0 +1,42 @@
+export const animalTypeDef = `
+type AnimalType {
+    id: Int!,
+    value: String!
+}
+type Animal {
+    id: Int!
+    name: String!,
+    user: String!,
+    breed: String!,
+    gender: Boolean!,
+    adoption: Boolean!,
+    birthdate: String!,
+    animal_type: AnimalType!
+    media: [String]!,
+}
+input AnimalTypeInput {
+    id: Int!,
+    value: String!
+}
+input AnimalInput {
+    name: String!,
+    user: String!,
+    breed: String!,
+    gender: Boolean!,
+    adoption: Boolean!,
+    birthdate: String!,
+    animal_type: AnimalTypeInput!
+    media: [String]!,
+}`;
+
+export const animalQueries = `
+    allAnimals: [Animal]!
+    allAnimalTypes: [AnimalType]!
+    animalById(id: Int!): Animal!
+`;
+
+export const animalMutations = `
+    createAnimal(animal: AnimalInput!): Animal!
+    updateAnimal(id: Int!, animal: AnimalInput!): Animal!
+    deleteAnimal(id: Int!): Animal!
+`;
